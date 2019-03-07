3b84a4acf2ac6719581e0d4fb47c087930354814
Michael Kriese <michael.kriese@visualon.de>
2019-03-07 17:22:16 +0100

    build: fix path

--- tools/webpack/webpack.common.js
+++ tools/webpack/webpack.common.js
@@ -3,43 +3,44 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
-                })
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /node_modules/,
-                query: {
-                  declaration: false,
-                }
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                use: ['file-loader', 'image-webpack-loader?bypassOnDebug'],
-                include: path.resolve(__dirname, '../../assets/img')
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new ExtractTextPlugin('styles.css')
-    ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, '../../dist'),
-        library: 'Xonomy',
-        libraryExport: 'default',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    }
+  context: path.resolve(__dirname, '../../'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+        query: {
+          declaration: false
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: ['file-loader', 'image-webpack-loader?bypassOnDebug'],
+        include: path.resolve(__dirname, '../../assets/img')
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(['../../dist'], { allowExternal: true }),
+    new ExtractTextPlugin('styles.css')
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../../dist'),
+    library: 'Xonomy',
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  }
 };
